1442bc3b8e06ecd0be4a7961434d5cc652c1f366
idan <idanelbaz@gmail.com>
2019-07-15 11:47:51 +0300

    mpre

--- api/google.route.js
+++ api/google.route.js
@@ -14,3 +14,11 @@ router.get('/getPos', async(req, res) => {
     var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat},${pos.lng}&radius=1500&type=park&keyword=dog&key=AIzaSyCrVxVPta_TOsFatlYL7vOx_stAJNlV8ws`)
     res.json(placesResult.data.results)
 })
+
+// router.get('/getPhoto', async(req, res) => {
+//     const idan = req.query;
+//     console.log('test', idan)
+//     var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${idan.ref}&key=AIzaSyCrVxVPta_TOsFatlYL7vOx_stAJNlV8ws`)
+//     console.log(placesResult.data.)
+//     res.json(placesResult.data)
+// })
