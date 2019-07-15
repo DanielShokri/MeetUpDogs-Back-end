e4d60b079dcd7109e177ede9bf9a75b08fdbf33c
chen mordechai <chen100030@gmail.com>
2019-07-15 09:02:23 +0300

    cors

--- server.js
+++ server.js
@@ -10,16 +10,16 @@ const bodyParser = require('body-parser')
 const session = require('express-session')
 const cors = require('cors')
 
+console.log('testing lla')
 
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['https://localhost:8080'],
+    origin: ['http://localhost:8080'],
     credentials: true
 }));
-
 
 app.use(session({
     secret: 'puki muki',
@@ -30,14 +30,11 @@ app.use(session({
     }
 }))
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-
-
-
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+// });
 
 
 const dogRoute = require('./api/dog.route');
