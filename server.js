6b056cdf69fb2c57d88ed42632be31c246d6ec9a
chen mordechai <chen100030@gmail.com>
2019-07-16 18:33:28 +0300

    server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json())
 app.use(cookieParser());
 
 app.use(cors({
-    origin: ['http://localhost:8080','http://localhost:8081'],
+    origin: ['http://localhost:8080','http://localhost:8081','http://localhost:8082'],
     credentials: true
 }));
 
