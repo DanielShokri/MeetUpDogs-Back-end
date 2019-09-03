5b7dc27f2497d485141c099362d3edb1ea31014a
chen mordechai <chen100030@gmail.com>
2019-09-03 10:36:11 +0300

    google key

--- api/google.route.js
+++ api/google.route.js
@@ -11,13 +11,16 @@ module.exports = router
 
 router.get('/getPos', async(req, res) => {
     const pos = req.query;
-    var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat},${pos.lng}&radius=2000&type=park&keyword=dog&key=AIzaSyDg8EFZZ2HBS2X6aqRzwRwfEbPO9SJLPfc`)
+    var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos.lat},${pos.lng}&radius=2000&type=park&keyword=dog&key=`)
     res.json(placesResult.data.results)
 })
 
 router.get('/getDistance', async(req, res) => {
     const distance = req.query;
-    var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${distance.dist}&destinations=${distance.userdist}&key=AIzaSyDg8EFZZ2HBS2X6aqRzwRwfEbPO9SJLPfc`)
+    var placesResult = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${distance.dist}&destinations=${distance.userdist}&key=`)
         // console.log(placesResult.data.rows[0])
     res.json(placesResult.data.rows[0])
 })
+
+
+//AIzaSyDg8EFZZ2HBS2X6aqRzwRwfEbPO9SJLPfc
